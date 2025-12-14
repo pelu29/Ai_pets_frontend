0b326e2621efc8fdb35963d15b7a6c8be4636b84
Darwin <floresjack74@gmail.com>
2025-12-14 12:02:32 -0500

    Definiendo rutas para el navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { Nosotros } from './components/nosotros/nosotros';
 import { Contacto } from './components/contacto/contacto';
 
 export const routes: Routes = [
+    {path: '', pathMatch:'full', component:Inicio},
     {path: 'inicio', component:Inicio},
     {path: 'nosotros', component:Nosotros},
     {path: 'contacto', component:Contacto}
