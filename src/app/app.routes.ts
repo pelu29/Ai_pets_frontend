70ae987fd24e17aa24ae5ab0be57fb7a8c034958
Darwin <floresjack74@gmail.com>
2025-12-14 11:57:44 -0500

    Componentes principales y diseños iniciales creados con exito.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Inicio } from './components/inicio/inicio';
+import { Nosotros } from './components/nosotros/nosotros';
+import { Contacto } from './components/contacto/contacto';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'inicio', component:Inicio},
+    {path: 'nosotros', component:Nosotros},
+    {path: 'contacto', component:Contacto}
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './shared/navbar/navbar';
+import { Footer } from './shared/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('ai_pets_fr');
+  
 }
