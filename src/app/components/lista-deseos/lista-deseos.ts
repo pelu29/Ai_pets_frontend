4ae2d17401ed3cc29238eeddfc9ec9accea4c997
shirocaya <p4m304r0j4z@gmail.com>
2025-12-18 08:46:31 -0500

    Introduciendo a la rama

---
+++ src/app/components/lista-deseos/lista-deseos.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-deseos',
+  imports: [],
+  templateUrl: './lista-deseos.html',
+  styleUrl: './lista-deseos.css',
+})
+export class ListaDeseos {
+
+}
