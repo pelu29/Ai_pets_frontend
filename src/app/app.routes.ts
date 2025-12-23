a00f3c83862fadacff56ab0f8d767cdcb3af31cd
FjaimeF <jaimepercca2@gmail.com>
2025-12-23 04:29:24 -0500

    filtros

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Inicio } from './components/inicio/inicio';
 import { Nosotros } from './components/nosotros/nosotros';
 import { Contacto } from './components/contacto/contacto';
+import { ProductListComponent } from './components/productos-list/productos-list';
 
 export const routes: Routes = [
     {path: '', pathMatch:'full', component:Inicio},
     {path: 'inicio', component:Inicio},
     {path: 'nosotros', component:Nosotros},
-    {path: 'contacto', component:Contacto}
+    {path: 'contacto', component:Contacto},
+    {path: 'productos', component:ProductListComponent}
 ];
