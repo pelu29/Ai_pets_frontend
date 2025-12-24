3f8d41c871e6878b152b94d68ab0172ecab39931
Blackpro666 <carlosrioespejo4@gmail.com>
2025-12-24 08:08:05 -0500

    Configuración de rutas y componente shop

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Inicio } from './components/inicio/inicio';
 import { Nosotros } from './components/nosotros/nosotros';
 import { Contacto } from './components/contacto/contacto';
+import { ShopUno } from './components/shop-uno/shop-uno';
 
 export const routes: Routes = [
     {path: '', pathMatch:'full', component:Inicio},
     {path: 'inicio', component:Inicio},
     {path: 'nosotros', component:Nosotros},
-    {path: 'contacto', component:Contacto}
+    {path: 'contacto', component:Contacto},
+    {path: 'shop-uno', component:ShopUno},
 ];
